0ad228c0089cf29f1bc79046e4ea604ad7700a94
Kathy Yost <kathyiyost@gmail.com>
2025-04-24 17:44:57 -0700

    Add project files.

---
+++ AssignmentManagement.Core/AssignmentService.cs
@@ -0,0 +1,26 @@
+﻿namespace AssignmentManagement.Core
+{
+    public class AssignmentService
+    {
+        private readonly List<Assignment> assignments = new();
+
+        public void AddAssignment(Assignment assignment)
+        {
+            assignments.Add(assignment);
+        }
+
+        public List<Assignment> ListAll()
+        {
+            return new List<Assignment>(assignments);
+        }
+
+        public List<Assignment> ListIncomplete()
+        {
+            return assignments
+                .Where(a => !a.IsCompleted)
+                .ToList();
+            // TODO: Return only assignments where IsCompleted is false
+            //throw new NotImplementedException();
+        }
+    }
+}
